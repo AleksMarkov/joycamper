97b6b85dac550225ebc5d7dc0e35d9a7a0f58e6d
AleksMarkov <137721438+AleksMarkov@users.noreply.github.com>
2024-08-03 01:53:29 -0400

    check

--- src/pages/WelcomePage/WelcomePage.jsx
+++ src/pages/WelcomePage/WelcomePage.jsx
@@ -5,8 +5,8 @@ import {
   ButtonLeft,
   ButtonRight,
 } from './WelcomePage.styled.jsx';
-import FooterPage from 'components/FooterPage/FooterPage';
 import HomePage from 'components/HomePage/HomePage';
+import FooterPage from 'components/FooterPage/FooterPage';
 import { Link } from 'react-router-dom';
 
 const WelcomePage = () => {
