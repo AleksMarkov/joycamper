113853744689d037c6fb5c6dcf4cee636ba674fd
AleksMarkov <137721438+AleksMarkov@users.noreply.github.com>
2024-09-22 02:23:51 -0400

    upd

--- src/redux/adverts/advertsOperation.js
+++ src/redux/adverts/advertsOperation.js
@@ -1,17 +1,3 @@
-// import { createAsyncThunk } from '@reduxjs/toolkit';
-// import { getAllAdverts } from '../../api/adverts';
-
-// export const fetchAllAdverts = createAsyncThunk(
-//   'adverts/fetchAll',
-//   async (_, { rejectWithValue }) => {
-//     try {
-//       const data = await getAllAdverts();
-//       return data;
-//     } catch (error) {
-//       return rejectWithValue(error.message);
-//     }
-//   }
-// );
 import { createAsyncThunk } from '@reduxjs/toolkit';
 import { getAllCampers } from '../../api/adverts'; // Измените на getAllCampers
 
